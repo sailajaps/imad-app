d55cf512ce4751ef24e9c1740d0d296fefd467d6
sailajaps <30761539+sailajaps@users.noreply.github.com>
2017-08-09 14:35:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/', function (req, res) {
+app.get('/a.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'a.html'));
 });
 
